5cedc7c25ebf4a49789cd8dce561a0ee99b8127a
Oscar Mendoza <osuka42@gmail.com>
2019-12-14 12:04:13 -0600

    Updates

---
+++ precache-manifest.9e87687405b89387f3a1964d17a282c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac0a6a89592a69590d93659c2d7e3ba",
+    "url": "/ssml-editor/index.html"
+  },
+  {
+    "revision": "2d180e613b0ef7abc5c2",
+    "url": "/ssml-editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "08a623590e5713381eae",
+    "url": "/ssml-editor/static/js/2.a1fed77d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ssml-editor/static/js/2.a1fed77d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d180e613b0ef7abc5c2",
+    "url": "/ssml-editor/static/js/main.77dbdc74.chunk.js"
+  },
+  {
+    "revision": "194eb46c503d5e972962",
+    "url": "/ssml-editor/static/js/runtime-main.a83de348.js"
+  }
+]);
